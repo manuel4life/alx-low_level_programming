d784088d827f2b4003bf1a736dc16227a2ac5582
manuel4life <allotey4eva@gmail.com>
2022-06-02 23:20:19 +0000

    commit for size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 
-printf("Size of a char: %ld bytes(s)\n", sizeof(a));
-printf("Size of a int: %ld bytes(s)\n", sizeof(b));
-printf("Size of a long int: %ld bytes(s)\n", sizeof(c));
-printf("Size of a long long int: %lu bytes(s)\n", sizeof(d));
-printf("Size of a float: %ld bytes(s)\n", sizeof(e));
+printf("Size of a char: %ld bytes(s)\n", sizeof(char));
+printf("Size of a int: %ld bytes(s)\n", sizeof(int));
+printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %ld bytes(s)\n", sizeof(float));
 
 	return (0);
 }
