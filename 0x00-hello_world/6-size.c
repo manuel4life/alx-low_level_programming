c528f3a9bc6a5cfda5afc1fe5903d02c2a62c736
manuel4life <allotey4eva@gmail.com>
2022-06-02 23:18:27 +0000

    commit for size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,6 @@
 
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
 
 printf("Size of a char: %ld bytes(s)\n", sizeof(a));
 printf("Size of a int: %ld bytes(s)\n", sizeof(b));
