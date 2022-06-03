6a5b0d39d24ed711cb99695742a823935b8b49e4
manuel4life <allotey4eva@gmail.com>
2022-06-03 23:26:09 +0000

    task 1 commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,9 +20,14 @@ int main(void)
 	if (lastn > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 	else if (lastn == 0)
+	{
 		printf("Last digit %d is %d and is 0\n", n, lastn);
+	}
+
 	else if (lastn < 6)
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
+	}
 
 	return (0);
 }
