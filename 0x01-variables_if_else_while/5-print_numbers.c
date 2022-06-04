9fca723cca17b52813f6b8c750543f765fdca27a
manuel4life <allotey4eva@gmail.com>
2022-06-04 00:39:49 +0000

    printing in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10 ; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
