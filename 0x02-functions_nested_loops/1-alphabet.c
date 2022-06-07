3980bbf9faf705a0bf3276b2943f8dc020eabd3c
manuel4life <allotey4eva@gmail.com>
2022-06-07 21:53:25 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print alphabet
+ *
+ * Return: 0 Always
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
