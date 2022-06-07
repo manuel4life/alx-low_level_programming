75491ff7f4aa5c577d54482f089d8c5426e9841c
manuel4life <allotey4eva@gmail.com>
2022-06-07 23:46:36 +0000

    lowerupper

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that will check for letters in upper or lowercase
+ * @c: letter input
+ * Return: 1
+ */
+
+int _isalpha(int c);
+{
+	if (((c >= 'a') && (c <= 'z') || ((c >= 'A') && (c <= 'Z')))
+			return (1);
+			else
+	return (0);
