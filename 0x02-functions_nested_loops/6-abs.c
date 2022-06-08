c8f89b21e37011093b44aad702194d7912c51608
manuel4life <allotey4eva@gmail.com>
2022-06-08 09:35:29 +0000

    _abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,11 @@
 
 /**
  * _abs - function that computes the value of an integer
- * @int: integer input
+ * @a: integer input
  * Return: absolute value
  */
 
-int _abs(int)
+int _abs(int a)
 {
 	if (a > 0 || a == 0)
 	{
