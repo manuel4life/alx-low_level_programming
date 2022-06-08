6fd166b2e070aab32fc37aa68e18785213eae1e2
manuel4life <allotey4eva@gmail.com>
2022-06-08 13:49:27 +0000

    counting numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print natural numbers from n t0 98
+ * @n: starting input
+ * Return: 0 Always
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
