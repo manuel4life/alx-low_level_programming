0f14d8511659f8aa6191101dcbb5bca405b38e36
manuel4life <allotey4eva@gmail.com>
2022-06-10 11:12:03 +0000

    sqaure

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,11 +12,19 @@ void print_square(int size)
 	int b;
 
 	if (size <= 0)
-		_putchar('\n');
-	for (a = 0; a < size; a++)
 	{
-		for (b = 0; a < size; b++)
-			_putchar('#');
 		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			_putchar('#');
+			for (b = 2; b <= size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
