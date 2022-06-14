89a848a881ab18ff9ffd63a0f5f38b273a4fdf90
manuel4life <allotey4eva@gmail.com>
2022-06-14 09:36:39 +0000

    print elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - a function that prints element
+ * @a: first integer
+ * @n: second integer
+ * Return: Always 0
+ */
+
+void print_array(int *a, int n)
+{
+	int k = 0;
+
+	while (k < n)
+	{
+		printf("%d", a[k]);
+		if (k < (n - 1))
+			printf(", ");
+		k++;
+	}
+	_putchar('\n');
+}
