c0a12f104dd2af4a4265a06c971b92865bcc3298
manuel4life <allotey4eva@gmail.com>
2022-06-16 20:59:05 +0000

    leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _leet - a function that encodes
+ * @n: string
+ * Return: Always 0
+ */
+
+char _leet(char *n)
+{
+	int k;
+	int j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (k = 0; n[k] != '\0'; k++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[k] == s1[j])
+			{
+				n[k] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
+
