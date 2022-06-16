f7c2d080a092ad07b769a9909b100a3a50d0e792
manuel4life <allotey4eva@gmail.com>
2022-06-16 20:18:18 +0000

    string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that prints  uppercase
+ * @n: string
+ * Return: Always 0
+ */
+
+char *string_toupper(char *n)
+{
+	int k;
+
+	for (i = 0; n[k] != '\0'; k++)
+	{
+		if (n[k] >= 'a' && n[k] <= 'z')
+			n[k] = n[k] - 32;
+	}
+	return (n);
+}
