7cb4690eab4efee78cf6bf448aaf73ae67fee187
manuel4life <allotey4eva@gmail.com>
2022-06-20 23:24:18 +0000

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - search for set of bytes in a string
+ * @s: checking string
+ * @accept: string for checking
+ * Return: the bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length;
+	int i;
+	int k;
+
+	length = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (k = 0; accept[k] != '\0' && accept[k] != s[i]; j++)
+			;
+		if (s[i] == accept[k])
+			length++;
+		if (accept[j] == '\0')
+			return (length);
+	}
+	return (length);
+}
