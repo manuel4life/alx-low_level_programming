da9684aeb8896f4f2e213087a93637560da2a93b
manuel4life <allotey4eva@gmail.com>
2022-06-20 23:26:07 +0000

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (k = 0; accept[k] != '\0' && accept[k] != s[i]; j++)
-			;
+
 		if (s[i] == accept[k])
 			length++;
 		if (accept[j] == '\0')
