ee8dea2c1ef4dec2a9f0bb17319f0403b6418c66
manuel4life <allotey4eva@gmail.com>
2022-06-21 11:24:01 +0000

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints chessboard
+ * @a: first string
+ * Return: Always 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (k = 0; k < 8; k++)
+			_putchar(a[i][k]);
+		_putchar('\n');
+	}
+}
