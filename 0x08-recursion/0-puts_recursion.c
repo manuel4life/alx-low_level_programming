a2e1a4ed2abe2bed9777ab91701a843ade578485
manuel4life <allotey4eva@gmail.com>
2022-06-22 21:28:06 +0000

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string
+ * @s: string input
+ * Return: string
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	i++;
+}
