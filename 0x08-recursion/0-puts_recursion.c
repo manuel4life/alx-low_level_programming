7651e6fe6414f408a1a1b6d5f331d4f4e1361663
manuel4life <allotey4eva@gmail.com>
2022-06-22 21:34:08 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		i++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(++s);
 }
