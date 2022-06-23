2c91580a3a3a132c8999482ab973ec04439f0c4f
manuel4life <allotey4eva@gmail.com>
2022-06-23 20:32:58 +0000

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int perfectSquare(int k, int j);
+int perfectSquare(int n, int j);
 /**
  * _sqrt_recursion - a function that returns a natural
  * square root of a number
@@ -21,12 +21,12 @@ int _sqrt_recursion(int n)
  * Return: square root
  */
 
-int perfectSquare(int k, int j)
+int perfectSquare(int n, int j)
 {
 	if (j < 1)
 		return (-1);
-	else if (j * j == k)
+	else if (j * j == n)
 		return (j);
 	else
-		return (perfectSquare(k, j - 1));
+		return (perfectSquare(n, j - 1));
 }
