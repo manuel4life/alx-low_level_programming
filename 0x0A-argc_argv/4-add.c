22802f16b987abf0e94b3e08662e131dc52df2f9
manuel4life <allotey4eva@gmail.com>
2022-06-25 23:28:09 +0000

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - function that adds two numbers
  * @argc: counter
@@ -10,20 +11,28 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	unsigned int i, sum, num;
 
-	if (argc < 1)
+	sum = 0;
+
+	if (argc < 3)
+	{
+	printf("%d\n", 0);
 	return (0);
-	for (i = 1; i < argc; i++)
-	{
-	if (!atoi(argv[i]))
-	{
-		printf("%s\n", "Error");
-		return (1);
 	}
-	sum += atoi(argv[i]);
+	while (argc-- && argc > 0)
+	{
+	for (i = 0; argv[argc][i] != '\0'; i++)
+	{
+	if (!(isdigit(argv[argc][i])))
+	{
+	printf("Error\n");
+	return (1);
+	}
+	}
+	num = atoi(argv[argc]);
+	sum += num;
 	}
 	printf("%d\n", sum);
-
-	return (0);
+	return (sum);
 }
