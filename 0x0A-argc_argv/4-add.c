b3f0ee83b05bfb9ebe0d64f6bfbf3d8e8920aa5e
manuel4life <allotey4eva@gmail.com>
2022-06-25 23:16:18 +0000

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,22 +8,22 @@
  * Return: Always 0
  */
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-	int i = 1, sum = 0;
+	int i, sum = 0;
 
 	if (argc < 1)
-		return (0);
-
+	return (0);
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
-		{
-			printf("Error");
-			return (1);
-		}
-		sum += atoi(argv[i]);
+	if (!atoi(argv[i]))
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
+
 	return (0);
 }
