258e5fe4eddfc8e0e34fbde4384c3ec1e316f5cb
manuel4life <allotey4eva@gmail.com>
2022-06-25 22:00:19 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: vlaue
+ * Return: 0 Always
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
