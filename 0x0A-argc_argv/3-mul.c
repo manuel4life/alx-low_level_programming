97baf07fa737de1989662d01513cbaeb51eb3309
manuel4life <allotey4eva@gmail.com>
2022-06-25 22:34:17 +0000

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: value
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int 1, val = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		val *= atoi(argv[i]);
+	}
+	printf("%d\n", val);
+	return (0);
+}
