e6cf40c19b2aeb16d0a07add4f709abb08d3274e
manuel4life <allotey4eva@gmail.com>
2022-07-04 22:13:49 +0000

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of file from which the progeame is complied
+ * Return: Nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
