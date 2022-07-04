5ec43f64b018d9fd80c2d8ecec474614960e168e
manuel4life <allotey4eva@gmail.com>
2022-07-04 23:39:42 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct of information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: who owns the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
