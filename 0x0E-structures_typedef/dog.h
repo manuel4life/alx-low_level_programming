4b489f22c34788b8bacbb651c2cbf79cc5b48585
manuel4life <allotey4eva@gmail.com>
2022-07-05 00:43:13 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,7 @@ struct dog
 /**
  * dog - typedef for dog struct
  */
-typedef struct dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
