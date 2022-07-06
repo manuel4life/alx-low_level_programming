9852058d09f988f13574a09faf8ae60c671b6f76
manuel4life <allotey4eva@gmail.com>
2022-07-06 22:11:30 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * print_name - a function that prints name
@@ -11,8 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-	{
-		f(name);
-	}
+		if (name && f)
+			f(name);
 }
