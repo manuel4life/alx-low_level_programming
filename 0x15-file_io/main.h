229e2a156116e558d35c4fce9e8f6b3bc4032080
manuel4life <allotey4eva@gmail.com>
2022-07-26 21:17:24 +0000

    correction

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
+void close_new(int f_n);
 #endif
