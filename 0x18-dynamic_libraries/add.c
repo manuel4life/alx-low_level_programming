26721cc8a34a883ff2ffad3bffbac33eca0721ac
manuel4life <allotey4eva@gmail.com>
2022-09-06 10:12:45 -0400

    C files

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * add - a function that adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
